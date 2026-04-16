a5c4a6696484ea2621f353158ef263ff4d0cf05a
laurence <laurencefluciano@gmail.com>
2026-04-16 15:52:48 -0300

    feat: adiciona definição de VideoMetadata para controle de dados do arquivo

---
+++ src/video.metadata.ts
@@ -0,0 +1,14 @@
+export type VideoMetadata = {
+    size: number,
+    type: AllowVideoTypes
+}
+
+
+const ALLOWED_EXTENSIONS = [".mp4", ".mov", ".webm"] as const;
+
+export type AllowVideoTypes = typeof ALLOWED_EXTENSIONS[number];
+
+export function isAllowedVideoType(type: string): type is AllowVideoTypes {
+    return ALLOWED_EXTENSIONS.includes(type as AllowVideoTypes);
+}
+
